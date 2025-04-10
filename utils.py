a94a9adf6d066768630014e217a03fecd53702fb
friedameissner <119932754+friedameissner@users.noreply.github.com>
2025-04-10 16:04:08 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -203,7 +203,7 @@ def zugehoerigkeit(codebook, data):
         argmin: Vektor mit Indizes der nächsten Codebooks.
         error: Mittlerer Fehler über alle gegebenen Daten.
     """
-    dists = np.sqrt(np.sum((codebook[:,None]-data)**2, axis=-1)).T
+    dists = pointwise_sq(data, codebook)
     argmin = np.argmin(dists, axis=-1)
     error = 0
     for i in range(len(argmin)):
